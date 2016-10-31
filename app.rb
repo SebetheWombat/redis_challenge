e444e8ced2709963213f3c28e93e160d47f83184
Sebe Keller <sebethkeller@gmail.com>
2016-10-31 13:26:14 -0400

    Check that content is not already in redis db

--- app.rb
+++ app.rb
@@ -1,9 +1,6 @@
-#TODO: Ensure only one of each file will be published no matter how many times app is run
-
 require "rubygems"
 require "open-uri"
 require "nokogiri"
-require "HTTParty"
 require "zip"
 require "redis"
 
@@ -13,21 +10,29 @@ def download_zip(url,zip_name,redis)
 	uri_zip = URI.parse(url)
 	result = Net::HTTP.get_response(uri_zip)
 
+	#Write zip file to local zip file under same name as original
 	new_zip = open(zip_name, "w")
 	new_zip.write(result.body)
 	new_zip.close
 
 	Zip::File.open(new_zip.path) do |zip_file|  
-  		zip_file.each do |entry|   
-    		puts "Extracting... #{entry}"
-    		redis.lpush("NEWS_XML",zip_file.read(entry))
+  		zip_file.each do |entry|   	
+    		#Checks if xml file has already been entered before adding content to redis list
+    		if redis.get(entry).nil?
+    			redis.set(entry,"content exists")
+	    		puts "Extracting... #{entry}"
+	    		content = zip_file.read(entry)
+	    		redis.lpush("NEWS_XML",content)
+	    	end
   		end
 	end	
 end
 
 def locate_zip(redis)
+	#parse site for a tags
 	page = Nokogiri::HTML(open("http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/"))
 	urls = page.css("a")
+	#for each a tag parse zip file link
 	urls.each do |url|
 		zip_link = url["href"]
 		#Ensures only zip files that have not yet been downloaded are downloaded and pushed to redis list
